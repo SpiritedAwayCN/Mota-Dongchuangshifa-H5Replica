1277478f60c146ccbf4700a969f2da34542f7552
SpiritedAwayCN <541845219@qq.com>
2024-01-07 21:52:45 +0800

    modified version

--- project/data.js
+++ project/data.js
@@ -441,7 +441,7 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 	"firstData": {
 		"title": "魔塔-东窗事发",
 		"name": "dongchuangshifa",
-		"version": "Ver 0.8.0",
+		"version": "Ver 0.8.1",
 		"floorId": "MT0",
 		"hero": {
 			"image": "brave.png",
